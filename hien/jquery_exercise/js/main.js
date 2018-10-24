0a5ee0eaa5396e867c3e2ca230c9ba034c846e1f
Ngo Le Huy Hien <hien.ngo160203@vnuk.edu.vn>
2018-10-24 11:47:40 +0700

    Add Delete Button

---
+++ hien/jquery_exercise/js/main.js
@@ -0,0 +1,9 @@
+// function clicked() {
+//     var input_value = document.getElementById('inputText').value;
+//     var h = document.createElement("H2");
+//     var t = document.createTextNode(input_value);
+//     h.appendChild(t);
+//     document.body.appendChild(h);
+// }
+
+// document.getElementById('button').addEventListener('click', clicked);
--- hien/js_add_elements/js/main.js
+++ hien/js_add_elements/js/main.js
@@ -3,7 +3,21 @@ function clicked() {
     var h = document.createElement("H2");
     var t = document.createTextNode(input_value);
     h.appendChild(t);
+
+    var button = document.createElement("input");
+    button.type = "button";
+    button.value = "Delete";
+    button.class ='delete';
+    button.onclick = function(){
+    	$(this).closest('h2').remove();
+    };
+
+    h.appendChild(button);
     document.body.appendChild(h);
 }
+
+// function deleteThis(){
+// 	$(this).closest('.h2').remove();
+// }
 
 document.getElementById('button').addEventListener('click', clicked);
