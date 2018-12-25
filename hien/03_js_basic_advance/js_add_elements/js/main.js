387fb27931273706c91ee77042f45ae4958d0bb2
Ngo Le Huy Hien <hien.ngo160203@vnuk.edu.vn>
2018-12-25 16:36:55 +0700

    Fixed JS Add Element

--- hien/03_js_basic_advance/js_add_elements/js/main.js
+++ hien/03_js_basic_advance/js_add_elements/js/main.js
@@ -4,16 +4,23 @@ function clicked() {
     var t = document.createTextNode(input_value);
     h.appendChild(t);
 
-    var button = document.createElement("input");
-    button.type = "button";
-    button.value = "Delete";
-    button.class ='delete';
-    button.onclick = function(){
-    	$(this).closest('h2').remove();
-    };
+    if (input_value === '') {
+        alert("Please input text!");
+    } else {
+        var button = document.createElement("input");
+        button.type = "button";
+        button.value = "Delete";
+        button.class ='delete';
+        button.onclick = function(){
+            $(this).closest('h2').remove();
+        };
 
-    h.appendChild(button);
-    document.body.appendChild(h);
+        h.appendChild(button);
+        document.body.appendChild(h);
+    }
+    document.getElementById('inputText').value = '';
+
+    
 }
 
 // function deleteThis(){
