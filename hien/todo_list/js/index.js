39d601cf13231991efe23d7b8158f8d00f11f868
Ngo Le Huy Hien <hien.ngo160203@vnuk.edu.vn>
2018-12-25 15:53:37 +0700

    Add 60 es6 exercises

--- hien/todo_list/js/index.js
+++ hien/todo_list/js/index.js
@@ -2,18 +2,63 @@ new Vue({
 	el: "#app",
 	data: {
 		todos: [
-			{ text: "Go to school", done: false },
-			{ text: "Travel", done: false },
-			{ text: "Play ABC", done: false },
-			{ text: "Learn something", done: false }
+			{ id: "1", text: "Go to school", done: false },
+			{ id: "2", text: "Travel", done: false },
+			{ id: "3", text: "Play ABC", done: false },
+			{ id: "4", text: "Learn something", done: false },
+			{ id: "5", text: "Happy Birthday", done: false },
+			{ id: "6", text: "Something tomorow", done: false },
+			{ id: "7", text: "Sing something", done: false }
 		]
 	},
-	methods: {
-		toggle: function(todo){
-			todo.done = !todo.done
+
+	filters: {
+		pluralize: function (n) {
+			return n === 1 ? 'todo' : 'todos'
 		}
+	},
+
+	methods: {
+		toggle: function (todo) {
+			todo.done = !todo.done
+		},
+
+		remove: function (todo) {
+			this.todos.splice(this.todos.indexOf(todo), 1)
+		},
+		deleteAll: function () {
+			this.todos = null
+		},
+		edit: function (todo) {
+			this.beforeEditCache = todo.text
+			this.editedTodo = todo
+
+			var e = document.getElementById('editText');
+			if(e.style.display == 'block')
+				e.style.display = 'none';
+			else
+				e.style.display = 'block';
+		},
+
+		doneEdit: function (todo) {
+			if (!this.editedTodo) {
+				return
+			}
+			this.editedTodo = null
+			todo.title = todo.text.trim()
+			if (!todo.text) {
+				this.removeTodo(todo)
+			}
+		},
+		cancelEdit: function (todo) {
+			this.editedTodo = null
+			todo.title = this.beforeEditCache
+			document.getElementById("edit").style.display = "none";
+		},
 	},
 	created: () => {
 		console.log('todos ', this);
 	}
+
+
 })
